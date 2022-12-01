3a0ae6b429b153c3b60761a2287e6435cf306424
Maxwell Smith <64322148+borksson@users.noreply.github.com>
2022-12-01 14:47:53 -0700

    made good progress

---
+++ raster_viewer.py
@@ -0,0 +1,22 @@
+import rasterio
+from rasterio.plot import show
+
+# from minecraftstuff import MinecraftShape
+# from mcpi.minecraft import Minecraft
+# from mcpi import block
+
+# fp = r'rasters/timp.tif'
+# src = rasterio.open(fp)
+
+# array = src.read()[0]
+
+# max = array.max()
+# min = array.min()
+
+# print(array.shape, max, min)
+
+# with open("array.txt", "w") as f:
+#     for row in array:
+#         for item in row:
+#             f.write(str(item) + " ")
+#         f.write("\n")
