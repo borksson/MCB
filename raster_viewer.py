1ea27302b489821c31e4f699f0139e58dfe6c22d
marky <NA>
2022-12-02 22:00:36 -0700

    Done

--- raster_viewer.py
+++ raster_viewer.py
@@ -5,8 +5,11 @@ import anvil
 
 import math
 
-fp = r'rasters/timp.tif'
+from tqdm import tqdm
+
+fp = r'rasters/grand.tif'
 src = rasterio.open(fp)
+show(src)
 
 array = src.read()[0]
 
@@ -21,18 +24,20 @@ print(array.shape, max, min)
 #             f.write(str(item) + " ")
 #         f.write("\n")
 
-region = anvil.Region.from_file("/home/borkson/.minecraft/saves/build_test_1_15 (1)/region/r.0.0.mca")
-# You can also provide the region file name instead of the object
-print(region)
-chunk = anvil.Chunk.from_region(region, 0, 0)
+region_folder = "C:/Users/aaron/AppData/Roaming/.minecraft/saves/timp_build/region/"
 
-# If `section` is not provided, will get it from the y coords
-# and assume it's global
-block = chunk.get_block(0, 3, 0)
+# region = anvil.Region.from_file("/home/borkson/.minecraft/saves/build_test_1_15 (1)/region/r.0.0.mca")
+# # You can also provide the region file name instead of the object
+# print(region)
+# chunk = anvil.Chunk.from_region(region, 0, 0)
 
-print(block) # <Block(minecraft:air)>
-print(block.id) # air
-print(block.properties) # {}
+# # If `section` is not provided, will get it from the y coords
+# # and assume it's global
+# block = chunk.get_block(0, 3, 0)
+
+# print(block) # <Block(minecraft:air)>
+# print(block.id) # air
+# print(block.properties) # {}
 
 region = anvil.EmptyRegion(0, 0)
 
@@ -42,12 +47,18 @@ dirt = anvil.Block('minecraft', 'dirt')
 
 # Make a 16x16x16 cube of either stone or dirt blocks
 
-for z in range(512):
+ratio = math.floor(array.shape[0]/512)
+
+for z in tqdm(range(512)):
     for x in range(512):
-        val = math.floor((array[x][z]*10)-(min*10))
+        val = math.floor(((array[x*ratio][z*ratio])-(min))/(max/256))
+        #print(val)
         if val > 255:
             val = 255
-        region.set_block(stone, x, val, z)
+        elif val <0 :
+            val = 0
+        for y in range(val):
+            region.set_block(stone, x, y, z)
 
 
-region.save("/home/borkson/.minecraft/saves/build_test_1_15 (1)/region/r.0.0.mca")
+region.save(region_folder+"r.0.0.mca")
